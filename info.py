47fec8d81544d9649dc007718829bbe449e1eea7
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-06-29 12:35:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ AUTH_REQ_CHANNEL = [int(ch) for ch in AUTH_REQ_CHANNEL.strip().split()] if AUTH_
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ' -1002765580351'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', ' -1002765580351'))
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://files.catbox.moe/a46151.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
