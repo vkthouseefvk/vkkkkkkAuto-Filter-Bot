8b5627beb7acf70df3bb472bd99f2171593aaa2c
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-06-30 10:29:02 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Telegram - @SilentXBotz")
+    return web.json_response("Telegram - @Infinity_Movies2")
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
