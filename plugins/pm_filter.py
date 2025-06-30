f1318bafb73a224efcb476c51f30bd7d9ffb47f5
Sadula Rushidhar <sadularushidhar+1@outlook.com>
2025-06-30 17:36:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1346,15 +1346,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.edit_message_media(
-		query.message.chat.id, 
-		query.message.id, 
-		InputMediaPhoto(random.choice(PICS))
-	)
-	    await query.message.edit_text(
-		    text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
-		    reply_markup=reply_markup,
-		    parse_mode=enums.ParseMode.HTML
-	    )
+            query.message.chat.id, 
+            query.message.id, 
+            InputMediaPhoto(random.choice(PICS))
+        )
+        await query.message.edit_text(
+            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
   
     elif query.data == "give_trial":
         try:
